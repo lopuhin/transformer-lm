5f66e124f2847a93127103f02931b7b81c55a2c7
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-03-17 22:19:04 +0300

    Fix training sentencepiece model

--- lm/data.py
+++ lm/data.py
@@ -5,8 +5,9 @@ import sentencepiece as spm
 import tqdm
 
 
-END_OF_SENTENCE = '</s>'
-END_OF_TEXT = '</text>'
+UNK = '<unk>'
+END_OF_LINE = '<endofline>'
+END_OF_TEXT = '<endoftext>'
 
 
 def sp_train():
@@ -19,24 +20,29 @@ def sp_train():
                         '(will be used as-is if exists)')
     arg('sp_model_prefix', help='path (prefix) to output sentencepiece model')
     arg('--vocab-size', type=int, default=50000)
+    arg('--character-coverage', type=float, default=1.0)
     args = parser.parse_args()
 
     sp_text = Path(args.sp_text)
-    if not sp_text.exists():
+    if sp_text.exists():
+        print(f'Using existing "{sp_text}", remove and re-run if it is stale.')
+    else:
         paths = []
-        for corpus_root in map(Path, args.corpus):
+        print(f'Reading corpora: {args.corpora}')
+        for corpus_root in map(Path, args.corpora):
             train_root = corpus_root / 'train'
-            paths = list(train_root.glob('**/*.txt'))
-            if not paths:
+            corpus_paths = list(train_root.glob('**/*.txt'))
+            if not corpus_paths:
                 parser.error('Corpus train split {train_root} looks empty, '
                              'no text files found')
+            paths.extend(corpus_paths)
         try:
             with sp_text.open('wt', encoding='utf8') as sp_text_file:
                 for path in tqdm.tqdm(
                         paths, desc='building sentencepiece input'):
                     with path.open('rt', encoding='utf8') as f:
                         for line in f.readlines():
-                            if line != '\n':
+                            if line.strip():
                                 sp_text_file.write(line)
         except Exception:
             if sp_text.exists():
@@ -48,10 +54,12 @@ def sp_train():
         f'--model_prefix={args.sp_model_prefix}',
         f'--vocab_size={args.vocab_size}',
         f'--model_type=bpe',
-        f'--bos_id=-1',
         f'--max_sentence_length=16384',
-        f'--eos_piece={END_OF_SENTENCE}',
-        f'--control_symbols={END_OF_TEXT}',
+        f'--bos_id=-1',
+        f'--eos_id=-1',
+        f'--unk_piece={UNK}',
+        f'--control_symbols={END_OF_LINE},{END_OF_TEXT}',
+        f'--character_coverage={args.character_coverage}',
     ]))
 
 
