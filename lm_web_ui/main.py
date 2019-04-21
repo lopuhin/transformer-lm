93d4f77c4ef7a8775b0a4bd8bc4a9511ba3e51d8
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-04-21 13:02:00 +0300

    Add csv download

--- lm_web_ui/main.py
+++ lm_web_ui/main.py
@@ -1,6 +1,9 @@
 import argparse
+import base64
+import csv
+import io
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 import aiohttp_jinja2
 from aiohttp import web
@@ -22,13 +25,30 @@ def index(request):
     if request.query.get('predict_next_token'):
         next_top_k = model.get_next_top_k(tokenize(text), top_k=10)
         ctx['next_token_prediction'] = next_top_k
+        ctx['next_token_prediction_csv'] = to_csv_data_url(next_top_k)
     elif request.query.get('score_occurred'):
-        ctx['occurred_scores'] = model.get_occurred_log_probs(tokenize(text))
+        occurred_scores = model.get_occurred_log_probs(tokenize(text))
+        ctx['occurred_scores'] = occurred_scores
+        ctx['occurred_scores_csv'] = to_csv_data_url(occurred_scores)
     return ctx
 
 
 def tokenize(text: str) -> List[str]:
-    return [app['model'].END_OF_TEXT] + app['model'].tokenize(text)
+    tokens = [app['model'].END_OF_TEXT] + app['model'].tokenize(text)
+    tokens = tokens[:app['model'].model.hparams.n_ctx]
+    return tokens
+
+
+def to_csv_data_url(data: List[Tuple[float, str]]) -> str:
+    """ Return data url with base64-encoded csv.
+    """
+    f = io.StringIO()
+    writer = csv.writer(f)
+    # reorder columns to match the UI
+    writer.writerow(['token', 'log_prob'])
+    writer.writerows([token, log_prob] for log_prob, token in data)
+    b64data = base64.b64encode(f.getvalue().encode('utf8')).decode('ascii')
+    return f'data:text/csv;base64,{b64data}'
 
 
 def main():
