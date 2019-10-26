8ec52a702284a0feee438b8a70226ed343645230
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-10-26 14:07:13 +0300

    Add softmax_t argument
    Does not seem useful? Also should double-check if this is the correct
    term here.

--- lm/inference.py
+++ lm/inference.py
@@ -44,37 +44,42 @@ class ModelWrapper:
     def id_to_token(self, token_id: int) -> str:
         return self.sp_model.IdToPiece(int(token_id))
 
-    def get_log_probs(self, tokens: List[str]) -> torch.Tensor:
+    def get_log_probs(
+            self, tokens: List[str], softmax_t: float = 1.0,
+            ) -> torch.Tensor:
         """ Return a tensor with shape (len(tokens), len(self.sp_model)),
         with log-probabilities for tokens after each token in tokens.
         If this is a start of the text, you may want to prepend END_OF_TEXT:
         model.get_log_probs([model.END_OF_TEXT] + tokens).
         Use model.tokenize to obtain tokens.
+        softmax_t is softmax temperature.
         """
         assert len(tokens) <= self.model.hparams.n_ctx  # TODO
         ids = [self.token_to_id(t) for t in tokens]
         ctx = torch.LongTensor(ids).unsqueeze(0)
         with torch.no_grad():
             logits = self.model(ctx)['logits'].squeeze(0)
-            return torch.log_softmax(logits, dim=1)
+            return torch.log_softmax(logits * softmax_t, dim=1)
 
     def get_occurred_log_probs(
-            self, tokens: List[str]) -> List[Tuple[float, str]]:
+            self, tokens: List[str], softmax_t: float = 1.0,
+            ) -> List[Tuple[float, str]]:
         """ Return a list of log probs of actually occurred tokens,
         starting from the second.
         """
-        log_probs = self.get_log_probs(tokens)
+        log_probs = self.get_log_probs(tokens, softmax_t=softmax_t)
         out = []
         for idx, token in enumerate(tokens[1:]):
             out.append((float(log_probs[idx, self.token_to_id(token)]), token))
         return out
 
     def get_next_top_k(
-            self, tokens: List[str], top_k: int) -> List[Tuple[float, str]]:
+            self, tokens: List[str], top_k: int, softmax_t: float = 1.0,
+            ) -> List[Tuple[float, str]]:
         """ Return a list of top k tuples of log prob and token,
         for what would come after the last token.
         """
-        next_log_probs = self.get_log_probs(tokens)[-1]
+        next_log_probs = self.get_log_probs(tokens, softmax_t=softmax_t)[-1]
         return sorted([(float(next_log_probs[i]), self.id_to_token(i))
                        for i in next_log_probs.argsort()[-top_k:]],
                       reverse=True)
